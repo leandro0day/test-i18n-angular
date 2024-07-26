5e461661c1893f99601e3c933858fa40ff930f44
leandroMatusIoon <leandromatus@ioon.it>
2024-07-26 16:43:39 -0600

    chore: Add @angular/localize to tsconfig files and package.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'test-i18n-angular';
+  title = $localize`Hello, How are you...`;
+  name = '';
 }
